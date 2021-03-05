1fcff46e4244bbe9b91c1dc3ecef3af756aacdb8
Ccourter11 <courtercalvin@yahoo.com>
2021-03-05 14:19:47 -0600

    created data for api, got KandyKorner.js rendering correctly and created a location component with some modules

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,15 @@
+// My Main App Component
+
+import React from "react";
+
+
+export const KandyKorner = () => (
+    <>
+        <h2>KandyKorner of Middle Tennessee</h2>
+        <small>Sour and Sweet!</small>
+        <address>
+            <div>Visit Us at one of the many Nashville Locations</div>
+            <div>Nashville,Murfreesboro and Lebanon!</div>
+        </address>
+    </>
+)
