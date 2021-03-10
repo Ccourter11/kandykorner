e6663004f1a27665ce67e7cebad3af0abc35bc13
Ccourter11 <courtercalvin@yahoo.com>
2021-03-10 06:58:03 -0600

    alteration

--- src/components/employees/EmployeeCard.js
+++ src/components/employees/EmployeeCard.js
@@ -4,7 +4,7 @@ import "./Employee.css";
 export const EmployeeCard = ({ employee }) => (
     <section className="employee">
         <h2>{employee.name}</h2>
-        <h4>Postion: {employee.position}</h4>
+        <h4>Postion : {employee.position}</h4>
         <p><b>City:</b> {employee.city}</p>
     </section>
 )
