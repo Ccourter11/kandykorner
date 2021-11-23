6ea044765c36d8c270b0737c3b1635532ca41098
Ccourter11 <courtercalvin@yahoo.com>
2021-11-23 15:02:17 -0600

    feat: fixed bug on product card and cleaned up employees card

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -3,7 +3,7 @@ import React from "react"
 export const ProductCard = ({ product, productType }) => (
     <div className="product">
       <h2 className="product__name">{product.name}</h2>
-      <div className="product__price">Price: ${product.price.toFixed(2)}</div>
-      <div className="product__type">Type: {productType.type}</div>
+      <div className="product__price">Price: ${product.price}</div>
+      <div className="product__type">Type: {product.productType}</div>
     </div>
   )
